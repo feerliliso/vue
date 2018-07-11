d688475653e1b6899bd60fa12df75faafa4701d2
feerliliso <feerliliso@hotmail.com>
2018-07-11 21:28:14 +0800

    add new data
    rt

--- index.js
+++ index.js
@@ -4,3 +4,31 @@ var app = new Vue({
     message: 'this is my first vue '
   }
 });
+app.message = 'I have changed the data'
+
+var app2 = new Vue({
+  el:'#app2',
+  data:{
+    message :'页面加载于'+ new Date().toLocaleString()
+  }
+})
+
+var app3  = new Vue({
+  el:'#app3',
+  data:{
+    seen:true
+  }
+})
+//app3.seen = false
+
+var app4 = new Vue({
+  el:'#app4',
+  data:{
+    todos:[
+      {text:'study javascript'},
+      {text:'day day hard'},
+      {text:'about 4 month , I will be a master'}
+    ]
+  }
+})
+app4.todos.push({text:'At here,a new line.'})
